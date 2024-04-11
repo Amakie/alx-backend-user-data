a7238e71f4358c7a1a80c81c7a0e61e23fabd0c3
Constance Amatey <consamate57@gmail.com>
2024-04-11 20:41:54 +0000

    added fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,9 @@ import logging
 from typing import List
 import re
 
+# fields to be redacted
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
