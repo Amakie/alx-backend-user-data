46441d79cc4f0cd2a702cf4ac16785214a710f5c
Constance Amatey <consamate57@gmail.com>
2024-04-21 21:00:23 +0000

    auth file

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3 env
+"""auth class"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+    def authorization_header(self, request=None) -> str:
+        """header function"""
+        if request is None:
+            return None
+
+    def current_user(self, request=None):
+        """current user function"""
+        if request is None:
+            return None
